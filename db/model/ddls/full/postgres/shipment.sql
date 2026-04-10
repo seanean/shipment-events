a4bd4155d6b5e8f4e09e0aac2c5360a97bb233e6
Sean Murphy <133802636+seanean@users.noreply.github.com>
2026-04-10 07:21:30 +0200

    moving/updating model location and sqls

---
+++ db/model/ddls/full/postgres/shipment.sql
@@ -0,0 +1,11 @@
+CREATE TABLE shipment (
+    shipment_uuid VARCHAR(255) NOT NULL,
+    shipment_business_id VARCHAR(255) NOT NULL,
+    shipment_technical_id VARCHAR(255) NOT NULL,
+    shipment_type VARCHAR(255) NOT NULL,
+    meta_source_event_ids VARCHAR(255),
+    meta_source_event_names VARCHAR(255),
+    meta_insert_timestamp TIMESTAMP,
+    meta_updated_timestamp TIMESTAMP,
+    PRIMARY KEY (shipment_uuid)
+);
