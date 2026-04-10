5921efec60a20106e7284ddc3f863958585a327a
Sean Murphy <133802636+seanean@users.noreply.github.com>
2026-04-10 17:15:51 +0200

    defining DDLs for raw and copying cur DDLs

---
+++ db/scripts/02-raw_shipment_product.sql
@@ -0,0 +1,10 @@
+USE DATABASE shipments;
+USE SCHEMA raw;
+
+CREATE TABLE shipment_product (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    payload JSONB NOT NULL,
+    event_id VARCHAR(255) NOT NULL,
+    event_timestamp TIMESTAMP NOT NULL,
+    event_name VARCHAR(255) NOT NULL
+);
