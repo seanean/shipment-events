780de006769878838278abf5abcc01574266ac01
Sean Murphy <133802636+seanean@users.noreply.github.com>
2026-04-12 11:27:27 +0200

    meta_insert_timestamp to raw queries

--- db/sql/raw/raw_shipment_products_insert.sql
+++ db/sql/raw/raw_shipment_products_insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO raw.shipment_products (event_id, event_timestamp, event_name, payload)
-VALUES (:event_id, :event_timestamp, :event_name, :payload)
+INSERT INTO raw.shipment_products (event_id, event_timestamp, event_name, payload, meta_insert_timestamp)
+VALUES (:event_id, :event_timestamp, :event_name, :payload, :meta_insert_timestamp)
