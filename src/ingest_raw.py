dd8e1aa2234376f16a936a416eabe9affb5908fe
Sean Murphy <133802636+seanean@users.noreply.github.com>
2026-04-12 13:34:46 +0200

    update ingestion logic to support quarantining bad payloads

--- src/ingest_raw.py
+++ src/ingest_raw.py
@@ -1,12 +1,14 @@
 import os
 import shutil
-from sqlalchemy.engine.row import RowMapping
 import yaml
 import logging
 import json
 from jsonschema import Draft202012Validator, ValidationError
 from db import get_engine
 from sqlalchemy import text
+from datetime import datetime, UTC
+import traceback
+from psycopg.types.json import Jsonb
 
 logger = logging.getLogger(__name__)
 
@@ -22,12 +24,20 @@ def ingest_raw(data, envt):
     for dirpath, dirnames, filenames in os.walk(config['landing_zone_pending_path']):
         for filename in filenames:
             filepath = os.path.join(dirpath, filename)
-            logger.info(f"Dirpath: {dirpath}, Dirnames: {dirnames}, Filenames: {filenames}, Filepath: {filepath}, Processing file: {filename}")
+            pending_dt_partition = os.path.basename(os.path.dirname(filepath))
+            logger.info(f"Processing file {filepath}")
 
             file = get_file(filepath)
-            validate_file(file, validator)
-            insert_to_raw_table(file, data)
-            archive_file(filename, filepath, config['landing_zone_archive_path'])
+
+            try:
+                validate_file(file, validator)
+            except ValidationError as e:
+                insert_to_table('quarantine', file, data, str(e), traceback.format_exc())
+                store_file(filename, filepath, pending_dt_partition, config['landing_zone_quarantine_path'])
+                continue
+
+            insert_to_table('raw', file, data)
+            store_file(filename, filepath, pending_dt_partition, config['landing_zone_archive_path'])
 
     cleanup_pending_lz(config['landing_zone_pending_path'])
     logger.info(f"Pending folder cleanup successful")
@@ -74,9 +84,10 @@ def validate_file(file, validator):
         logger.error(f"File: {file}")
         raise e
 
-def insert_to_raw_table(file, data):
-    logger.info(f"Inserting file into raw table for {data}")
-    insert_row = insert_row_builder(file, data)                        
+
+def insert_to_table(schema, file, data, error_message=None, traceback_message=None):
+    logger.info(f"Inserting file into {schema} table for {data}")
+    insert_row = insert_row_builder(schema, file, data, error_message, traceback_message)                        
     logger.info(f"Insert row builder successful")
     logger.debug(f"Insert row: {insert_row}")
 
@@ -84,55 +95,70 @@ def insert_to_raw_table(file, data):
     logger.info(f"Engine retrieved successfully")
     logger.debug(f"Engine: {engine}")
 
-    insert_qry = get_raw_insert_statement(data)
+    insert_qry = get_insert_statement(schema, data)
 
     with engine.begin() as conn:
         logger.info(f"Executing insert query")
         conn.execute(text(insert_qry),insert_row,)
         logger.info(f"Insert query executed successfully")
-        # temp execution just to see result in terminal
-        result = conn.execute(text(f"select * from raw.{data};")).mappings().all()
-        for i, row in enumerate(result):
-            logger.info(f"{data} Row {i}: {row}")
+        result = conn.execute(text(f"select count(1) from {schema}.{data};")).mappings().all()
+        logger.info(f"{schema}.{data} row count: {result[0]['count']}")
 
-
-def get_raw_insert_statement(data):
-    logger.info(f"Getting insert query for {data}")
-    path = f"db/sql/raw/raw_{data}_insert.sql"
+def get_insert_statement(schema, data):
+    logger.info(f"Getting insert query for {schema} {data}")
+    path = f"db/sql/{schema}/{schema}_{data}_insert.sql"
     with open(path, encoding="utf-8") as f:
         insert_qry = f.read()
         logger.info(f"Insert query retrieved successfully")
         logger.debug(f"Insert query: {insert_qry}")
         return insert_qry
 
-def insert_row_builder(file, data):
-    logger.info(f"Building insert row for {data}")
-    match data:
-        case "shipment_status":
-            return [{"payload": json.dumps(file), "event_id": file["event_id"],
+def insert_row_builder(schema, file, data, error_message, traceback_message):
+    logger.info(f"Building insert row for {schema} {data}")
+
+    meta_insert_timestamp = datetime.now(UTC)
+    match (schema, data):
+        case ("raw", "shipment_status"):
+            return {"payload": Jsonb(file), "event_id": file["event_id"],
                         "event_timestamp": file["event_timestamp"],
-                        "event_name": file["event_name"]}]
-        case "shipment_products":
-            return [{"payload": json.dumps(file), "event_id": file["event_id"],
+                        "event_name": file["event_name"],
+                        "meta_insert_timestamp": meta_insert_timestamp}
+        case ("raw", "shipment_products"):
+            return {"payload": Jsonb(file), "event_id": file["event_id"],
                         "event_timestamp": file["event_timestamp"],
-                        "event_name": file["event_name"]}]
+                        "event_name": file["event_name"],
+                        "meta_insert_timestamp": meta_insert_timestamp}
+        case ("quarantine", "shipment_status"):
+            return {"payload": Jsonb(file), "event_id": file["event_id"],
+                        "event_timestamp": file["event_timestamp"],
+                        "event_name": file["event_name"],
+                        "error_message": error_message,
+                        "traceback_message": traceback_message,
+                        "meta_insert_timestamp": meta_insert_timestamp}
+        case ("quarantine", "shipment_products"):
+            return {"payload": Jsonb(file), "event_id": file["event_id"],
+                        "event_timestamp": file["event_timestamp"],
+                        "event_name": file["event_name"],
+                        "error_message": error_message,
+                        "traceback_message": traceback_message,
+                        "meta_insert_timestamp": meta_insert_timestamp}
         case _:
-            logger.error(f"Whatcha talkin' bout Willis? Unknown raw table: {data}")
-            raise ValueError(f"Whatcha talkin' bout Willis? Unknown raw table: {data}")
+            logger.error(f"Whatcha talkin' bout Willis? Unknown {schema} and/or table: {data}")
+            raise ValueError(f"Whatcha talkin' bout Willis? Unknown {schema} and/or table: {data}")
 
-def archive_file(filename, filepath, archive_folder):
-    logger.info(f"Archiving file {filename} from {filepath} to {archive_folder}")
-    pending_dt_partition = os.path.basename(os.path.dirname(filepath))
-    archive_folder = os.path.join(archive_folder, pending_dt_partition)
+def store_file(filename, filepath, pending_dt_partition, target_folder):
+    target_partition_folder = os.path.join(target_folder, pending_dt_partition)
+    logger.info(f"Storing file {filename} from {filepath} to {target_partition_folder}")
     
-    if not os.path.exists(archive_folder):
-        logger.info(f"Archive folder {archive_folder} does not exist, creating it")
-        os.makedirs(archive_folder)
-        logger.info(f"Archive folder {archive_folder} created successfully")
+    if not os.path.exists(target_partition_folder):
+        logger.info(f"Target folder {target_partition_folder} does not exist, creating it")
+        os.makedirs(target_partition_folder)
+        logger.info(f"Target folder {target_partition_folder} created successfully")
 
-    destination_filepath = os.path.join(archive_folder, filename)
+    destination_filepath = os.path.join(target_partition_folder, filename)
     shutil.move(filepath, destination_filepath)
-    logger.info(f"File {filename} archived successfully to {destination_filepath}")
+    logger.info(f"File {filename} stored successfully to {destination_filepath}")
+
 
 def cleanup_pending_lz(pending_folder):
     logger.info(f"Cleaning up pending folder {pending_folder}")
@@ -143,11 +169,13 @@ def cleanup_pending_lz(pending_folder):
             logger.info(f"Pending folder {dirpath} removed successfully")
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--data", required=True, choices=["shipment_status", "shipment_products"], help="The data to ingest")
-    parser.add_argument("-e","--environment", default="dev", choices=["dev", "prod"], help="The environment to ingest from")
-    args = parser.parse_args()
-    ingest_raw(args.data, args.environment)
+    # to be created later when I want to add __init__.py and trigger via cli with args
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("-d", "--data", required=True, choices=["shipment_status", "shipment_products"], help="The data to ingest")
+    # parser.add_argument("-e","--environment", default="dev", choices=["dev", "prod"], help="The environment to ingest from")
+    # args = parser.parse_args()
+    # ingest_raw(args.data, args.environment)
+    print('hello world :)')
 
 if __name__ == "__main__":
     main()
