e06234e6aa379e55ac9c55d312bdd1b716a504ad
Sean Murphy <133802636+seanean@users.noreply.github.com>
2026-04-19 10:04:39 +0200

    updated cln insert logic for conflicts

--- db/sql/cln/cln_shipment_products_insert.sql
+++ db/sql/cln/cln_shipment_products_insert.sql
@@ -2,17 +2,26 @@ INSERT INTO cln.shipment_products (
     event_id,
     event_timestamp,
     event_name,
-    payload,
+    payload_cln,
     raw_offset_id,
     meta_insert_timestamp,
+    meta_update_timestamp,
     meta_source_file_path
 )
 VALUES (
     :event_id,
     :event_timestamp,
     :event_name,
-    :payload,
+    :payload_cln,
     :raw_offset_id,
     :meta_insert_timestamp,
+    :meta_update_timestamp,
     :meta_source_file_path
 )
+ON CONFLICT (event_id) DO UPDATE SET
+    payload_cln = excluded.payload_cln,
+    meta_update_timestamp = excluded.meta_insert_timestamp,
+    meta_source_file_path = excluded.meta_source_file_path,
+    raw_offset_id = excluded.raw_offset_id,
+    event_timestamp = excluded.event_timestamp,
+    event_name = excluded.event_name
