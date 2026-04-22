05bb6deebe190b238efb58a2de0fd442d9e1572d
Sean Murphy <133802636+seanean@users.noreply.github.com>
2026-04-22 18:17:47 +0200

    col name tweaks

--- db/scripts/init-ddl/02-cur_shipment_status.sql
+++ db/scripts/init-ddl/02-cur_shipment_status.sql
@@ -4,11 +4,12 @@ CREATE TABLE cur.shipment_status (
     shipment_status VARCHAR NOT NULL,
     shipment_status_tmst TIMESTAMPTZ NOT NULL,
     meta_source_event_names VARCHAR NOT NULL,
+    meta_source_latest_event_id VARCHAR NOT NULL,
     meta_source_event_id_lst VARCHAR NOT NULL,
     meta_source_file_path_lst VARCHAR NOT NULL,
     meta_root_business_key VARCHAR NOT NULL,
     meta_insert_timestamp TIMESTAMPTZ NOT NULL,
-    meta_updated_timestamp TIMESTAMPTZ,
+    meta_update_timestamp TIMESTAMPTZ,
     PRIMARY KEY (shipment_status_uuid)
 );
 ALTER TABLE cur.shipment_status OWNER TO shrw;
