dc7a0c3c5b9c617039d4e75f817c840718ec6fec
Sean Murphy <133802636+seanean@users.noreply.github.com>
2026-04-22 22:21:59 +0200

    lol after all that work to understand yaml arrays, i don't even need the curated tables

--- src/config_util.py
+++ src/config_util.py
@@ -21,7 +21,7 @@ class ResolvedConfig:
     quarantine_target_table: str
     cln_insert_sql_path: Path
     cln_target_table: str
-    curated_tables: list[dict[str, str | Path]]
+    cur_insert_sql_path: Path
 
 def get_config(data: str, config_path: Path) -> dict:
     logger.info(f"Getting config for {data} from {config_path}")
@@ -44,13 +44,7 @@ def resolve_config(loaded_config: dict[str, Any]) -> ResolvedConfig:
         quarantine_target_table=f'{loaded_config['quarantine_db_schema']}.{loaded_config['quarantine_table']}',
         cln_target_table=f'{loaded_config['cln_db_schema']}.{loaded_config['cln_table']}',
         cln_insert_sql_path=_REPO_ROOT_PATH / loaded_config['cln_insert_sql_path'],
+        cur_insert_sql_path=_REPO_ROOT_PATH / loaded_config['cur_insert_sql_path'],
         # if at some point it's needed, we can also resolve the raw_table and raw_db_schema parameters
-        curated_tables = [
-            {
-                'cur_target_table': f'{loaded_config['curated_db_schema']}.{cur_table['cur_table']}',
-                'cur_insert_sql_path': _REPO_ROOT_PATH / cur_table['cur_insert_sql_path']
-            }
-            for cur_table in loaded_config['curated_tables']
-        ]
     )
         
--- src/tests/test_config_util.py
+++ src/tests/test_config_util.py
@@ -5,7 +5,7 @@ from typing import Any
 
 # resolve_config tests
 def test_resolve_config() -> None:
-    loaded_config: dict[str, Any] = {
+    loaded_config: dict[str, str | Path] = {
         'lz_pending_path': 'landing-zone/pending/shipment_status',
         'lz_archive_path': 'landing-zone/archive/shipment_status',
         'lz_quarantine_path': 'landing-zone/quarantine/shipment_status',
@@ -19,17 +19,7 @@ def test_resolve_config() -> None:
         'cln_db_schema': 'cln',
         'cln_table': 'shipment_status',
         'cln_insert_sql_path': 'db/sql/cln/cln_shipment_status_insert.sql',
-        'curated_db_schema': 'cur',
-        'curated_tables': [
-            {
-                'cur_table': 'shipment_header',
-                'cur_insert_sql_path': 'db/sql/cur/shipment_status__cur_shipment_header_insert.sql',
-            },
-            {
-                'cur_table': 'shipment_status',
-                'cur_insert_sql_path': 'db/sql/cur/cur_shipment_status_insert.sql',
-            },
-        ]
+        'cur_insert_sql_path': 'db/sql/cur/batch_cur_shipment_products.sql'
     }
     result = resolve_config(loaded_config)
 
@@ -40,13 +30,10 @@ def test_resolve_config() -> None:
     assert isinstance(result.schema_path, Path)
     assert isinstance(result.raw_insert_sql_path, Path)
     assert isinstance(result.quarantine_insert_sql_path, Path)
+    assert isinstance(result.cur_insert_sql_path, Path)
     assert isinstance(result.raw_target_table, str)
     assert isinstance(result.quarantine_target_table, str)
     assert isinstance(result.cln_target_table, str)
-    assert isinstance(result.curated_tables, list)
-    assert all(isinstance(table, dict) for table in result.curated_tables)
-    assert all(isinstance(table['cur_target_table'], str) for table in result.curated_tables)
-    assert all(isinstance(table['cur_insert_sql_path'], Path) for table in result.curated_tables)
     # path checks
     assert result.lz_pending_path == _REPO_ROOT_PATH.joinpath(loaded_config['lz_pending_path'])
     assert result.lz_archive_path == _REPO_ROOT_PATH.joinpath(loaded_config['lz_archive_path'])
@@ -55,17 +42,11 @@ def test_resolve_config() -> None:
     assert result.raw_insert_sql_path == _REPO_ROOT_PATH.joinpath(loaded_config['raw_insert_sql_path'])
     assert result.quarantine_insert_sql_path == _REPO_ROOT_PATH.joinpath(loaded_config['quarantine_insert_sql_path'])
     assert result.cln_insert_sql_path == _REPO_ROOT_PATH.joinpath(loaded_config['cln_insert_sql_path'])
+    assert result.cur_insert_sql_path == _REPO_ROOT_PATH.joinpath(loaded_config['cur_insert_sql_path'])
     # table checks
     assert result.raw_target_table == f'{loaded_config['raw_db_schema']}.{loaded_config['raw_table']}'
     assert result.quarantine_target_table == f'{loaded_config['quarantine_db_schema']}.{loaded_config['quarantine_table']}'
     assert result.cln_target_table == f'{loaded_config['cln_db_schema']}.{loaded_config['cln_table']}'
-    assert result.curated_tables == [
-        {
-            'cur_target_table': f'{loaded_config['curated_db_schema']}.{row['cur_table']}',
-            'cur_insert_sql_path': _REPO_ROOT_PATH / row['cur_insert_sql_path'],
-        }
-        for row in loaded_config['curated_tables']
-    ]
 
 
 def test_get_config_returns_dict(tmp_path: Path) -> None:
